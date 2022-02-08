5c080477a7f00424586b14bd8fc91d9f7413cc70
Bobby8249 <Bharat8249@gmail.com>
2022-02-08 12:40:11 +0530

    update output.tf

--- output.tf
+++ output.tf
@@ -1,10 +1,10 @@
 output "cloudwatch_metric_alarm_arn" {
   description = "The ARN of the Cloudwatch metric alarm."
-  value       = module.cloudwatch_metric_alarm_arn
+  value       = module.metric_alarm.cloudwatch_metric_alarm_arn
 }
 
 output "cloudwatch_metric_alarm_id" {
   description = "The ID of the Cloudwatch metric alarm."
-  value       = module.cloudwatch_metric_alarm_id
+  value       = module.metric_alarm.cloudwatch_metric_alarm_id
 }
 
