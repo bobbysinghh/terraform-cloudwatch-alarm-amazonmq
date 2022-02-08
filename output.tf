dcf00432e72e37ea75a5a2f1e5582a500ee7c847
Bobby8249 <Bharat8249@gmail.com>
2022-02-08 12:36:18 +0530

    initial commit

---
+++ output.tf
@@ -0,0 +1,10 @@
+output "cloudwatch_metric_alarm_arn" {
+  description = "The ARN of the Cloudwatch metric alarm."
+  value       = module.cloudwatch_metric_alarm_arn
+}
+
+output "cloudwatch_metric_alarm_id" {
+  description = "The ID of the Cloudwatch metric alarm."
+  value       = module.cloudwatch_metric_alarm_id
+}
+
